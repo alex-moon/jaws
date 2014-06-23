050102946125af6c4a6174926c07beeec88f4bcd
Alex Moon <alex.moon@tangentlabs.co.uk>
2014-06-23 12:35:40 +0100

    Move mapper to its own method in core :)

--- src/main/java/com/github/alex_moon/HomePage.java
+++ src/main/java/com/github/alex_moon/HomePage.java
@@ -4,21 +4,22 @@ import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 
-import com.amazonaws.auth.profile.ProfileCredentialsProvider;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 
-public class HomePage extends WebPage {    
-    static AmazonDynamoDBClient client = new AmazonDynamoDBClient(new ProfileCredentialsProvider());
+public class HomePage extends WebPage {
 	private static final long serialVersionUID = 1L;
 
 	public HomePage(final PageParameters parameters) {
 		super(parameters);
-		
-		DynamoDBMapper mapper = new DynamoDBMapper(client);
+
+		DynamoDBMapper mapper = WicketApplication.getMapper();
 
 		Term term = mapper.load(Term.class, "radical");
 
-		add(new Label("killer", term.toString()));
+		if (term == null) {
+			add(new Label("killer", "term not found :("));
+		} else {
+			add(new Label("killer", term.toString()));
+		}
     }
 }
--- src/main/java/com/github/alex_moon/WicketApplication.java
+++ src/main/java/com/github/alex_moon/WicketApplication.java
@@ -3,14 +3,16 @@ package com.github.alex_moon;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.protocol.http.WebApplication;
 
-/**
- * Application object for your web application.
- * If you want to run this application without deploying, run the Start class.
- * 
- * @see com.github.alex_moon.Start#main(String[])
- */
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+
 public class WicketApplication extends WebApplication
 {
+	private static DynamoDBMapper mapper;
 	/**
 	 * @see org.apache.wicket.Application#getHomePage()
 	 */
@@ -29,5 +31,18 @@ public class WicketApplication extends WebApplication
 		super.init();
 
 		// add your configuration here
+	}
+
+	public static DynamoDBMapper getMapper() {
+		if (mapper == null) {
+			AWSCredentialsProvider credentials = new ProfileCredentialsProvider();
+			AmazonDynamoDBClient client = new AmazonDynamoDBClient(credentials);
+			Region ireland = Region.getRegion(Regions.EU_WEST_1);
+			client.setRegion(ireland);
+
+			mapper = new DynamoDBMapper(client);
+		}
+
+		return mapper;
 	}
 }
