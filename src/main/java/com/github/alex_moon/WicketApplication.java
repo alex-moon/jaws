0da433be5d457cf88e100d0918153a0ff1f3bd2b
Alex Moon <alex.moon@tangentlabs.co.uk>
2014-06-25 00:36:06 +0100

    try-catch for profile credentials OR AssumeRole - this is the way to do it

--- src/main/java/com/github/alex_moon/WicketApplication.java
+++ src/main/java/com/github/alex_moon/WicketApplication.java
@@ -3,7 +3,7 @@ package com.github.alex_moon;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.protocol.http.WebApplication;
 
-import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.profile.ProfileCredentialsProvider;
 import com.amazonaws.regions.Region;
 import com.amazonaws.regions.Regions;
@@ -13,6 +13,7 @@ import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 public class WicketApplication extends WebApplication
 {
 	private static DynamoDBMapper mapper;
+	private static AmazonDynamoDBClient client;
 
 	/**
 	 * @see org.apache.wicket.Application#getHomePage()
@@ -33,14 +34,28 @@ public class WicketApplication extends WebApplication
 
 		// add your configuration here
 	}
+	
+	public static AmazonDynamoDBClient getClient() {
+		if (client == null) {
+			try {
+				// Dev uses profile credentials
+				AWSCredentials credentials = new ProfileCredentialsProvider().getCredentials();
+				client = new AmazonDynamoDBClient(credentials);
+			} catch (IllegalArgumentException e) {
+				// Live uses EC2 service role
+				client = new AmazonDynamoDBClient();
+			}
+
+			Region ireland = Region.getRegion(Regions.EU_WEST_1);
+			client.setRegion(ireland);
+		}
+		
+		return client;
+	}
 
 	public static DynamoDBMapper getMapper() {
 		if (mapper == null) {
-			AmazonDynamoDBClient client = new AmazonDynamoDBClient();
-			Region ireland = Region.getRegion(Regions.EU_WEST_1);
-			client.setRegion(ireland);
-
-			mapper = new DynamoDBMapper(client);
+			mapper = new DynamoDBMapper(getClient());
 		}
 
 		return mapper;
